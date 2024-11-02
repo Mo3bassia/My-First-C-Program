c8636414dbb669484ad671ff989e5d40c087c455
Mohamed Abassia <mo3bassia@gmail.com>
2024-11-02 05:59:36 +0200

    my 1st program with c++

---
+++ app.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    float num2 = 10.0;
+    double num3 = 10.19999999999999; // can store more than 6 digits after the decimal point and up to 15
+    float num4 = 10.19999999999999; // can't store more than 6 digits after the decimal point
+    string text = "Hello World!\nMohamed Atef\nFrontend Developer\n";
+    cout << "Hello World!\nMohamed Atef\nFrontend Developer" << endl << "Mo3bassia\n";
+    cout << text;
+    cout << num * 9 << endl;
+    cout << num2 * 9 << endl;
+    cout << num3 << endl;
+    cout << num4 << endl;
+    return 0;
+}
+/*
+    First Program in C++ 💪
+*/
